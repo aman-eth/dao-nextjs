491e20f77023c1cbc2b271244bef42d7349865b6
Aman Sharma <amans@geekyants.com>
2022-02-06 12:10:04 +0530

    fixed incorrect abi and added get functions that are called while booting up our website

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { Contract, ethers, utils } from "ethers";
+import { Contract, ethers, providers, utils } from "ethers";
 import Head from "next/head";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import {
   CRYPTODEVS_DAO_ABI,
@@ -20,6 +20,132 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [walletConnected, setWalletConnected] = useState(false);
   const web3ModalRef = useRef();
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("please switch to rinkeby network!");
+      throw new Error("please switch to rinkeby network!");
+    }
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const getDAOTreasuryBalance = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const balance = await provider.getBalance(
+        CRYPTODEVS_DAO_CONTRACT_ADDRESS,
+      );
+      setTreasuryBalance(balance.toString());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getUserNFTBalance = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = getCryptodevsNFTContractInstance(signer);
+      const balance = await nftContract.balanceOf(signer.getAddress());
+      setNftBalance(parseInt(balance).toString());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getNumProposalsInDAO = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const contract = getDAOContractInstance(provider);
+      const daoNumProposals = await contract.numProposals();
+      setNumProposals(daoNumProposals.toString());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const fetchAllProposals = async () => {
+    try {
+      const proposals = [];
+      for (let i = 0; i < numProposals; i++) {
+        const proposal = await fetchProposalById(i);
+        proposals.push(proposal);
+      }
+      setProposals(proposals);
+      return proposals;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const fetchProposalById = async (id) => {
+    try {
+      const provider = await getProviderOrSigner();
+      const daoContract = getDAOContractInstance(provider);
+      const proposal = await daoContract.proposals(id);
+      const parsedProposal = {
+        proposalId: id,
+        nftTokenId: proposal.nftTokenId.toString(),
+        deadline: new Date(parseInt(proposal.deadline.toString()) * 1000),
+        yayVotes: proposal.yayVotes.toString(),
+        nayVotes: proposal.nayVotes.toString(),
+        executed: proposal.executed,
+      };
+      return parsedProposal;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getDAOContractInstance = (providerOrSigner) => {
+    return new Contract(
+      CRYPTODEVS_DAO_CONTRACT_ADDRESS,
+      CRYPTODEVS_DAO_ABI,
+      providerOrSigner,
+    );
+  };
+
+  const getCryptodevsNFTContractInstance = (providerOrSigner) => {
+    return new Contract(
+      CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+      CRYPTODEVS_NFT_ABI,
+      providerOrSigner,
+    );
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet().then(() => {
+        getDAOTreasuryBalance();
+        getUserNFTBalance();
+        getNumProposalsInDAO();
+      });
+    }
+  }, [walletConnected]);
+
+  useEffect(() => {
+    if (selectedTab === "View Proposals") fetchAllProposals();
+  }, [selectedTab]);
 
   function renderTabs() {
     if (selectedTab === "Create Proposal") {
